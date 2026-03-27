df331a9a4a0f1da59425df5a1d34c3effd7dc120
Leontii <zzeroootwooo@gmail.com>
2026-03-27 17:27:13 +0200

    Add todo DTOs (TodoResponse, CreateTodoRequest, UpdateTodoRequest, SetCompletionRequest, PagedResponse)

--- DTOs/AuthDtos.cs
+++ DTOs/AuthDtos.cs
@@ -1,6 +1,44 @@
-﻿namespace TodoApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.DTOs
 {
-    public class AuthDtos
+    public class RegisterRequest
     {
+        [Required]
+        [EmailAddress]
+        [MaxLength(254)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(128)]
+        public string Password { get; set; } = string.Empty;
+
+        public string? DisplayName { get; set; }
+    }
+
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class AuthUserResponse
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string? DisplayName { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string TokenType { get; set; } = "Bearer";
+        public int ExpiresInSeconds { get; set; } = 3600;
+        public AuthUserResponse User { get; set; } = null!;
     }
 }
